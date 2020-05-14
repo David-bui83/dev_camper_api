fb4b341e31473802b732222c80ee1ee665ad177e
Mona Lisa <david.t.bui83@gmail.com>
2020-05-13 23:24:39 -0700

    setup mongodb and mongoose

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -19,4 +23,12 @@ if(process.env.NODE_ENV === 'development'){
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)});
+
+const server = app.listen(PORT, ()=>{console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)});
+
+// Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise)=>{
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
