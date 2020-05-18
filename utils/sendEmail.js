2e3e9396d7c6218f3dd3ec9dd13ed240ec3165bb
Mona Lisa <david.t.bui83@gmail.com>
2020-05-17 19:37:18 -0700

    added nodemailer

---
+++ utils/sendEmail.js
@@ -0,0 +1,51 @@
+const nodemailer = require('nodemailer');
+
+// const sendEmail = async (options) => {
+//   // Create transporter
+//   const transporter = nodemailer.createTransport({ 
+//     host: process.env.SMTP_HOST,
+//     port: process.env.SMTP_PORT,
+//     auth: {
+//       user: process.env.SMTP_EMAIL,
+//       user: process.env.SMTP_PASSWORD
+//     } 
+//   });
+
+//   // Email object to send email
+//   const message = {
+//     from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message
+//   }
+
+//   // Send mail
+//   const info = await transporter.sendMail(message);
+
+//   console.log('Message sent: %s', info.messageId);
+// };
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD
+    }
+  });
+
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log('Message sent: %s', info.messageId);
+};
+
+
+module.exports = sendEmail;
